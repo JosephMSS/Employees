5bdfc263718a8ea867213a44eba9c0f838e1b199
Joseph Morales Silva <josephzx1@hotmail.com>
2021-07-03 01:40:40 -0600

    Renombrar  ruta

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ const employeeRouter = require('./components/employee/network');
 const app=express()
 
 app.use(express.json())
-app.use("/",employeeRouter)
+app.use("/api/employee",employeeRouter)
 app.listen(3000,()=>{
     console.log('Listen http://localhost:3000');
 })
